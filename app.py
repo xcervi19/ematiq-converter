bad48e3783874247a246f647c0611221df134c74
Karel Cervicek <cervicekkarel@gmail.com>
2023-04-29 16:56:57 +0200

    Shutting down gracefully

--- app.py
+++ app.py
@@ -4,4 +4,17 @@ from currency_ws_client import CurrencyWebSocketClient
 if __name__ == "__main__":
     currency_websocket_client = CurrencyWebSocketClient()
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(currency_websocket_client.websocket_handler())
+    try:
+        loop.run_until_complete(currency_websocket_client.websocket_handler())
+    except KeyboardInterrupt:
+        print("Shutting down gracefully...")
+        # Cancel all running tasks
+        tasks = asyncio.all_tasks(loop)
+        for task in tasks:
+            task.cancel()
+        # Wait for tasks to be finished
+        loop.run_until_complete(asyncio.gather(*tasks, return_exceptions=True))
+    finally:
+        loop.close()
+
+
